3a11d3224cba9080b0dc3e3ab6206e90196e6000
Ahmad sabastian <138794399+sabs09@users.noreply.github.com>
2024-06-24 12:47:15 +0700

    LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AuthLayout from '../components/layouts/AuthLayout';
 import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
 
 const LoginPage = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault(); // Mencegah reload halaman
+    navigate('/HomeView');
   };
 
   return (
@@ -13,7 +21,7 @@ const LoginPage = () => {
       <div className='flex flex-col justify-center items-center gap-2'>
         <h1 className='text-4xl text-primary font-bold'>Selamat Datang Pawrents!</h1>
         <p>Daftar atau Masuk untuk mengakses semua fitur</p>
-        <form className='flex flex-col gap-2'>
+        <form className='flex flex-col gap-2' onSubmit={handleLogin}>
           <div className='border border-slate-300 rounded w-96'>
             <input
               type="text"
@@ -38,8 +46,8 @@ const LoginPage = () => {
           </div>
           <small className='text-primary cursor-pointer flex justify-end w-full'>Lupa password?</small>
           <div className='flex justify-between mt-10 px-2'>
-           <a href="src/pages/HomePage.jsxr" className='bg-[#FF8D75] rounded px-16 py-1 text-white'>Masuk</a> 
-            <button className='btn bg-primary hover:bg-primary/80 rounded px-16 text-white'>Daftar</button>
+            <button type="submit" className='bg-[#FF8D75] rounded px-16 py-1 text-white'>Masuk</button>
+            <button type="button" className='btn bg-primary hover:bg-primary/80 rounded px-16 text-white'>Daftar</button>
           </div>
         </form>
       </div>
